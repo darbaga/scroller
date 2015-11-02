b168d3c47fad55dadf7a70f79d1c2eea2fb62848
riovan <dhruvdropbox@gmail.com>
2015-11-02 10:48:00 +0530

    Modify weapon class

--- scroller/GameObjects.py
+++ scroller/GameObjects.py
@@ -12,5 +12,6 @@ class BaseGameObj(object):
 
 class Weapon(BaseGameObj):
     """Class for most weapons. If you need to add any more attributes / methods, just subclass / inherit this"""
-    def __init__
+    def __init__(self, name='', description='', getsound='', dropsound='', effects=[], swingsound='', equipsound=''):
+        super(Weapon, self).__init__(self, name=name, description=description, getsound=getsound, dropsound=dropsound, effects=effects)
         """This class is supposed to have everything BaseGameObj does, plus swingsound, equipSound, other weapon related things, with the self.swingsound=swingsound... Etc... COde added on the BaseGameObj's __init__ function."""
