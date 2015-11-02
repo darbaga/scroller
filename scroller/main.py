2905552c461a1af7be2b6c1de98231ae81aa99bd
riovan <dhruvdropbox@gmail.com>
2015-11-02 09:49:12 +0530

    add the glue code to make it all work

---
+++ scroller/main.py
@@ -0,0 +1,24 @@
+import pyglet
+
+import libaudioverse
+libaudioverse.initialize()
+s = libaudioverse.Simulation()
+s.set_output_device(-1)
+
+from tiles import Tile
+from map import Map
+from player import Player
+
+from sound import SoundLoader
+sounds = SoundLoader(s)
+
+default_tile = Tile(sound=sounds.load_sound('default'))
+impassable_tile = Tile(sound=sounds.load_sound('impassable'), impassable=True)
+
+Window = pyglet.window.Window()
+map = Map(default_tile, impassable_tile)
+player = Player(map)
+Window.push_handlers(player)
+
+if __name__ == '__main__':
+    pyglet.app.run()
