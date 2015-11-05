f0bb181d5e475b996887d0748c965b1f16de2b64
riovan <dhruvdropbox@gmail.com>
2015-11-05 17:42:44 +0530

    add a vector class

---
+++ scroller/vector.py
@@ -0,0 +1,11 @@
+class Vector(object):
+    __slots__=['x', 'y']
+    
+    def __init__(self, x=0.0, y=0.0):
+        self.x, self.y = x,y
+    def __add__(self, other):
+        return self.__class__(self.x+other.x, self.y+other.y)
+    def scale(self, num):
+        return self.__class__(self.x*num, self.y*num)
+    def __repr__(self):
+        return '({}, {})'.format(self.x, self.y)
