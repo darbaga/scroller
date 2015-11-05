8fddb266323a8129b3bc83635e2d00ff88937e9f
riovan <dhruvdropbox@gmail.com>
2015-11-05 18:40:07 +0530

    refactor movement system to use vectors, still buggy

--- scroller/player.py
+++ scroller/player.py
@@ -15,10 +15,13 @@ class Player(object):
         
         self.map = map
         self.position = Vector()
+        self.tile=self.map.get_tile((int(self.position.x), int(self.position.y)))
         
-        self.left_counter = 0
-        self.right_counter = 0
         self.KeyStateHandler = KeyStateHandler
+        
+        self.is_moving=False
+        self.movement_direction=Vector()
+        self.movement_speed=3.0
         
     def move_left(self):
         #our player has a list of position coordinates, while our map expects a tuple.
@@ -35,22 +38,36 @@ class Player(object):
             self.position.x+=1
     def on_key_press(self, symbol, modifiers):
         if symbol == key.LEFT:
-            self.left_counter+=50
+            self.is_moving=True
+            self.movement_direction.x=-1
         elif symbol==key.RIGHT:
-            self.right_counter+=50
+            self.is_moving=True
+            self.movement_direction.x=1
         elif symbol == key.C:
             print self.position
         elif symbol == key.H:
             print "You have %d out of %d hp, (%d percent)." %(self.hp, self.max_hp, self.hp/self.max_hp*100)
-
+            
+    def on_key_release(self, symbol, modifiers):
+        if symbol==key.LEFT or symbol==key.RIGHT:
+            self.is_moving=False
     def update(self, dt):
-        if self.left_counter>300:
-            self.move_left()
-            self.left_counter = 0
-        elif self.right_counter>300:
-            self.move_right()
-            self.right_counter = 0
-        elif self.KeyStateHandler[key.RIGHT]:
-            self.right_counter+=50
-        elif self.KeyStateHandler[key.LEFT]:
-            self.left_counter += 50
+        if self.is_moving:
+            self.position=self.position+self.movement_direction.scale(self.movement_speed*dt)
+        if self.tile!=self.map.get_tile((int(self.position.x), int(self.position.y))):
+            if not self.map.get_tile((int(self.position.x), int(self.position.y))).impassable:
+                self.map.get_tile((int(self.position.x), int(self.position.y))).collide(self)
+                self.tile=self.map.get_tile((int(self.position.x), int(self.position.y)))
+            else:
+                self.map.get_tile((int(self.position.x), int(self.position.y))).collide(self)
+        if not self.is_moving and self.position.x<int(self.position.x)+.5:
+            self.position.x=int(self.position.x)+.5
+#            self.move_left()
+#            self.left_counter = 0
+#        elif self.right_counter>300:
+#            self.move_right()
+#            self.right_counter = 0
+#        elif self.KeyStateHandler[key.RIGHT]:
+#            self.right_counter+=50
+#        elif self.KeyStateHandler[key.LEFT]:
+#            self.left_counter += 50
