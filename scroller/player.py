a8825abc9904a128b5de410fcfc765fc3f3b7535
riovan <dhruvdropbox@gmail.com>
2015-11-05 18:45:19 +0530

    remove unnessessary code from player.py

--- scroller/player.py
+++ scroller/player.py
@@ -17,25 +17,10 @@ class Player(object):
         self.position = Vector()
         self.tile=self.map.get_tile((int(self.position.x), int(self.position.y)))
         
-        self.KeyStateHandler = KeyStateHandler
-        
         self.is_moving=False
         self.movement_direction=Vector()
         self.movement_speed=3.0
         
-    def move_left(self):
-        #our player has a list of position coordinates, while our map expects a tuple.
-        if self.map.is_impassable((self.position.x-1, self.position.y)):
-            self.map.get_tile((self.position.x-1, self.position.y)).collide(self)
-        else:
-            self.map.get_tile((self.position.x-1, self.position.y)).collide(self)
-            self.position.x-=1
-    def move_right(self):
-        if self.map.is_impassable((self.position.x+1, self.position.y)):
-            self.map.get_tile((self.position.x+1, self.position.y)).collide(self)
-        else:
-            self.map.get_tile((self.position.x+1, self.position.y)).collide(self)
-            self.position.x+=1
     def on_key_press(self, symbol, modifiers):
         if symbol == key.LEFT:
             self.is_moving=True
@@ -55,19 +40,10 @@ class Player(object):
         if self.is_moving:
             self.position=self.position+self.movement_direction.scale(self.movement_speed*dt)
         if self.tile!=self.map.get_tile((int(self.position.x), int(self.position.y))):
-            if not self.map.get_tile((int(self.position.x), int(self.position.y))).impassable:
+            if not self.map.get_tile((int(self.position.x), int(self.position.y))).impasable:
                 self.map.get_tile((int(self.position.x), int(self.position.y))).collide(self)
                 self.tile=self.map.get_tile((int(self.position.x), int(self.position.y)))
             else:
                 self.map.get_tile((int(self.position.x), int(self.position.y))).collide(self)
         if not self.is_moving and self.position.x<int(self.position.x)+.5:
             self.position.x=int(self.position.x)+.5
-#            self.move_left()
-#            self.left_counter = 0
-#        elif self.right_counter>300:
-#            self.move_right()
-#            self.right_counter = 0
-#        elif self.KeyStateHandler[key.RIGHT]:
-#            self.right_counter+=50
-#        elif self.KeyStateHandler[key.LEFT]:
-#            self.left_counter += 50
